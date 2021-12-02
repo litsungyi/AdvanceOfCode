667170698165b55e298f42c21e1fd7e7999401e7
litsungyi <litsungyi@gmail.com>
2021-12-03 00:29:40 +0800

    feat: Add day-1-1

---
+++ 2021/day-01/day1-1.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const lines = fs.readFileSync('input.txt', 'utf-8').split(/\n/).map(line => parseInt(line));
+console.log(lines.filter( (line, index) => index == 0 || line > lines[index -1] ).length);
