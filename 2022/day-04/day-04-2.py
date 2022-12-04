072146682f9812f358185d4f4d3d8d91dcea3204
litsungyi <litsungyi@gmail.com>
2022-12-04 23:16:10 +0800

    feat: Add day04

---
+++ 2022/day-04/day-04-2.py
@@ -0,0 +1,16 @@
+import re
+
+PATTERN = r"(?P<start_1>[0-9]+)-(?P<end_1>[0-9]+),(?P<start_2>[0-9]+)-(?P<end_2>[0-9]+)"
+count = 0
+with open('./2022/day-04/input.txt', 'r') as input:
+    for line in [line.strip('\n') for line in input]:
+        matches = re.search(PATTERN, line)
+        start_1 = int(matches.group('start_1'))
+        end_1 = int(matches.group('end_1'))
+        start_2 = int(matches.group('start_2'))
+        end_2 = int(matches.group('end_2'))
+
+        if not start_2 > end_1 and not start_1 > end_2:
+            count += 1
+
+    print(count)
