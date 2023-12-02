31931f02d3e1a6a56a0034a8c63cf9412a2342ed
litsungyi <litsungyi@gmail.com>
2023-12-03 03:02:30 +0800

    fix: Uodate new template

--- new.sh
+++ new.sh
@@ -13,14 +13,14 @@ mkdir -p "${YEAR}/day-${DAY}"
 
 if [[ ! -e "${YEAR}/day-${DAY}/day-${DAY}-1.py" ]]
 then
-    echo "with open('./${YEAR}/day-${DAY}/test.txt', 'r') as input:
+    echo "with open('./${YEAR}/day-${DAY}/test-1.txt', 'r') as input:
     for line in [line.strip('\n') for line in input]:
         pass" >> "${YEAR}/day-${DAY}/day-${DAY}-1.py"
 fi
 
 if [[ ! -e "${YEAR}/day-${DAY}/day-${DAY}-2.py" ]]
 then
-    echo "with open('./${YEAR}/day-${DAY}/test.txt', 'r') as input:
+    echo "with open('./${YEAR}/day-${DAY}/test-2.txt', 'r') as input:
     for line in [line.strip('\n') for line in input]:
         pass" >> "${YEAR}/day-${DAY}/day-${DAY}-2.py"
 fi
@@ -30,7 +30,12 @@ then
     echo "" >> "${YEAR}/day-${DAY}/input.txt"
 fi
 
-if [[ ! -e "${YEAR}/day-${DAY}/test.txt" ]]
+if [[ ! -e "${YEAR}/day-${DAY}/test-1.txt" ]]
 then
-    echo "" >> "${YEAR}/day-${DAY}/test.txt"
+    echo "" >> "${YEAR}/day-${DAY}/test-1.txt"
+fi
+
+if [[ ! -e "${YEAR}/day-${DAY}/test-2.txt" ]]
+then
+    echo "" >> "${YEAR}/day-${DAY}/test-2.txt"
 fi
